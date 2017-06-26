78e75fd836a0989ee8fc8f80c61b8b3dbc62f9b8
XuYanci <tdw@tdwdeiMac.local>
2017-06-26 18:06:08 +0800

    commit some.

--- GLViewPagerViewControllerForSwift/GLViewPagerViewControllerForSwift/GLViewPagerViewController.swift
+++ GLViewPagerViewControllerForSwift/GLViewPagerViewControllerForSwift/GLViewPagerViewController.swift
@@ -8,29 +8,134 @@
 
 import UIKit
 
- 
-class GLViewPagerViewController: UIViewController {
+
+public enum GLIndicatorType:Int {
+    case GLIndicatorType_Line = 0
+    case GLIndicatorType_Rect = 1
+}
+
+public enum GLTabAnimationType:Int {
+     /* no animation */
+    case GLTabAnimationType_None = 0
+    /* animation while scrolling */
+    case GLTabAnimationType_WhileScrolling = 1
+    /* animation when ending */
+    case GLTabAnimationType_End = 2
+}
+
+
+
+// MARK: - Protocols 
+@objc public protocol GLViewPagerViewControllerDataSource:NSObjectProtocol {
+    @objc optional func numberOfTabsForViewPager(_ viewPager:GLViewPagerViewController)
+    @objc optional func viewForTabIndex(_ viewPager:GLViewPagerViewController, index: Int) -> UIView
+    @objc optional func contentViewControllerForTabAtIndex(_ viewPager:GLViewPagerViewController, index: Int) -> UIViewController
+    @objc optional func contentViewForTabAtIndex(_ viewPager:GLViewPagerViewController, index: Int) -> UIView
+}
+
+@objc public protocol GLViewPagerViewControllerDelegate:NSObjectProtocol {
+    @objc optional func didChangeTabToIndex(_ viewPager: GLViewPagerViewController, index: Int, fromTabIndex: Int)
+    @objc optional func willChangeTabToIndex(_ viewPager: GLViewPagerViewController, index: Int, fromTabIndex: Int, progress: CGFloat)
+    @objc optional func widthForTabIndex(_ viewPager: GLViewPagerViewController, index: Int) -> CGFloat
+}
+
+
+open class GLViewPagerViewController: UIViewController, UIPageViewControllerDataSource,UIPageViewControllerDelegate,UIScrollViewDelegate {
+    
+    
+    // MARK: - public properties
+    open var dataSource: GLViewPagerViewControllerDataSource!
+    
+    open var delegate: GLViewPagerViewControllerDelegate?
+    
+    open var indicatorColor: UIColor = UIColor.blue
+    
+    open var fixTabWidth: Bool = false
+    
+    open var tabWidth:CGFloat = 128.0
+    
+    open var tabHeight:CGFloat = 44.0
+    
+    open var indicatorHeight:CGFloat = 2.0
+    
+    open var indicatorWidth:CGFloat = 128.0
+    
+    open var fixIndicatorWidth:Bool = false
+    
+    open var padding:CGFloat = 0.0
+    
+    open var leadingPadding:CGFloat = 0.0
+    
+    open var trailingPadding:CGFloat = 0.0
+    
+    open var defaultDisplayPageIndex = 0
+    
+    open var animationTabDuration:CGFloat = 0.3
+    
+    open var tabAnimationType:GLTabAnimationType = GLTabAnimationType.GLTabAnimationType_None
+    
+    open var supportArabic:Bool = false
+    
+    // MARK: - ui properties
+    internal var pageViewController: UIPageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    internal var tabContentView: UIScrollView = UIScrollView()
+    internal var indicatorView: UIView = UIView();
+    
+    
+    // MARK: - cache properties
+    internal var _needsReload: Bool = false
+    internal var leftTabOffsetWidth: CGFloat = 0
+    internal var rightTabOffsetWidth: CGFloat = 0
+    internal var leftMinusCurrentWidth: CGFloat = 0
+    internal var rightMinusCurrentWidth: CGFloat = 0
+    internal var _currentPageIndex: Int = 0
+    internal var _enableTabAnimationWhileScrolling:Bool = false
+    internal var contentViewControllers:NSMutableArray = NSMutableArray()
+    internal var contentViews:NSMutableArray = NSMutableArray()
+    internal var tabViews:NSMutableArray = NSMutableArray()
+    
+    
+    
     // MARK: - Life cycle
-    override func viewDidLoad() {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.commonInit()
+    }
+    
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    
+    override open func awakeFromNib() {
+        super.awakeFromNib()
+        self.commonInit()
+    }
+    
+    override open func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override open func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    override func loadView() {
-    
+    override open func loadView() {
+        self.view = UIView(frame: UIScreen.main.bounds)
+        self.view.backgroundColor = UIColor.white
+        self.view .addSubview(self.tabContentView)
+        self.view .addSubview(self.pageViewController.view)
     }
     
-    override func viewWillLayoutSubviews() {
-        
+    override open func viewWillLayoutSubviews() {
+        self ._reloadDataIfNeed()
+        self ._layoutSubviews()
     }
-    
-    
     
     
     /*
@@ -43,12 +148,33 @@ class GLViewPagerViewController: UIViewController {
     }
     */
 
-    
 
-    
     // MARK: - Data Source
+    open func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return nil;
+    }
+    
+    
+    open func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return nil;
+    }
     
     // MARK: - Delegate
+    open func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        
+    }
+    
+    open func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        
+    }
+    
+    open  func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+    }
+    
+    open func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+    }
     
     // MARK: - User Events
     
--- GLViewPagerViewControllerForSwift/GLViewPagerViewControllerForSwift/ViewController.swift
+++ GLViewPagerViewControllerForSwift/GLViewPagerViewControllerForSwift/ViewController.swift
@@ -6,13 +6,70 @@
 //  Copyright © 2017年 Yanci. All rights reserved.
 //
 
+
+
+
 import UIKit
 
-class ViewController: UIViewController {
 
+class GLPresentViewController: UIViewController {
+    
+    var presentLabel: UILabel = UILabel()
+    
+    internal var _title : NSString = "Page Zero"
+    internal var _setupSubViews:Bool = false
+  
+    init(title : NSString) {
+        _title = title
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.presentLabel.text = self._title as String
+        self.presentLabel.sizeToFit()
+        self.view .addSubview(self.presentLabel)
+        self.presentLabel.center = self.view.center
+    }
+    
+    override func viewWillLayoutSubviews() {
+        self.presentLabel.center = self.view.center
+    }
+   
+    
+    
+}
+
+
+class ViewController: GLViewPagerViewController,GLViewPagerViewControllerDataSource,GLViewPagerViewControllerDelegate {
+
+    
+    
+    // MARK: - cache properties
+    var viewControllers: NSArray = NSArray()
+    var tabTitles: NSArray = NSArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.title = "Paged Tabs"
+        self.dataSource = self;
+        self.delegate = self;
+        self.padding = 10
+        self.leadingPadding = 10
+        self.trailingPadding = 10
+        self.defaultDisplayPageIndex = 0
+        self.tabAnimationType = GLTabAnimationType.GLTabAnimationType_WhileScrolling
+        self.indicatorColor = UIColor.init(colorLiteralRed: 255.0 / 255.0, green: 205.0 / 255.0, blue: 0.0, alpha: 1.0)
+        self.supportArabic = false
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
