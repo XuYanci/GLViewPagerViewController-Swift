88841f9efbe4b7e0f828c5b497bf180a7a4f0676
XuYanci <grandy.wind@gmail.com>
2017-06-26 15:53:21 +0800

    add viewcontroller structure

---
+++ GLViewPagerViewControllerForSwift/GLViewPagerViewControllerForSwift/GLViewPagerViewController.swift
@@ -0,0 +1,89 @@
+//
+//  GLViewPagerViewController.swift
+//  GLViewPagerViewControllerForSwift
+//
+//  Created by Yanci on 2017/6/26.
+//  Copyright © 2017年 Yanci. All rights reserved.
+//
+
+import UIKit
+
+ 
+class GLViewPagerViewController: UIViewController {
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func loadView() {
+    
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+
+    
+    // MARK: - Data Source
+    
+    // MARK: - Delegate
+    
+    // MARK: - User Events
+    
+    // MARK: - Functions 
+    func commonInit () {
+        
+    }
+    
+    func setDataSource() -> Void {
+        
+    }
+    
+    func setDelegate() -> Void {
+        
+    }
+    
+    func reloadData() -> Void {
+        
+    }
+    
+    func _setNeedsReload() -> Void {
+        
+    }
+    
+    func _reloadDataIfNeed() -> Void {
+        
+    }
+    
+    func _layoutSubviews() -> Void {
+        
+    }
+    
+    // MARK: - Notification 
+    
+    // MARK: - Getter and Setter 
+    
+    
+}
